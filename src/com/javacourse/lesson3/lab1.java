d17253e2615b22542b6667e48cfcb10f8d1ba932
Ivan Gn <103774899+IGnx64@users.noreply.github.com>
2022-04-17 16:39:51 +0300

    Update lab1.java

--- src/com/javacourse/lesson3/lab1.java
+++ src/com/javacourse/lesson3/lab1.java
@@ -1,16 +1,15 @@
-package com.company;
+import java.util.Scanner;
 
-public class lab1 {
-    public static void main(String[] args) {
+class Sum {
+    public static void main(String [] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Введите первое число A = ");
         int a = scan.nextInt();
-        System.out.print("Введите второе число B = ");
+        System.out.print("Введите первое число B = ");
         int b = scan.nextInt();
-        System.out.print("Введите третье число C = ");
+        System.out.print("Введите первое число C = ");
         int c = scan.nextInt();
-
-        System.out.println("A + B + C = " + (a + b + c));
+        System.out.println("A + B + C = " + ( a + b + c ));
     }
 }
