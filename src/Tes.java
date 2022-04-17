7cb432a4968a907de454e724c114052855022a8f
IGnx64 <i.ganinx86@gmail.com>
2022-04-17 16:15:52 +0300

    Лаб раб1_Ганин ИА.

---
+++ src/Tes.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class lab1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("A = ");
+        int a = scan.nextInt();
+        System.out.print("B = ");
+        int b = scan.nextInt();
+        System.out.print("C = ");
+        int c = scan.nextInt();
+
+        System.out.println("A + B + C = " + (a + b + c));
+    }
+}
